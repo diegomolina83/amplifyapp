580c0680bdd17b6199b32cc054566303195697f4
diegomolina83 <diegomolina83@gmail.com>
2020-10-24 17:35:20 +0200

    changes for v2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hola Desde Diego
         </a>
       </header>
     </div>
